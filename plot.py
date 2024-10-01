1ec6b9f6225b4f00d61cf9377d753e866f7888a9
bai@werkgymnasium.eu <bai@werkgymnasium.eu>
2024-10-01 19:04:25 +0200

    removed last majortick

--- plot.py
+++ plot.py
@@ -10,9 +10,20 @@ plt.rc('font', family='serif')
 # Konvertiere cm in inch
 cm_to_inch = 1 / 2.54
 
+
+
+x_min = -2
+x_max = 2 # Maximaler x-Wert
+y_min = -4
+y_max = 4 # Maximaler y-Wert
+
+grid_spacing_x = 1
+grid_spacing_y = 1
+
+
 # Die Breite der x-Achse in cm beträgt 3 cm und die Höhe der y-Achse 5 cm
-axis_width_cm = 3  # 3 cm Breite
-axis_height_cm = 5  # 5 cm Höhe
+axis_width_cm = x_max - x_min # 3 cm Breite
+axis_height_cm = y_max - y_min  # 5 cm Höhe
 axis_width_inch = axis_width_cm * cm_to_inch  # 3 cm in inch
 axis_height_inch = axis_height_cm * cm_to_inch  # 5 cm in inch
 
@@ -31,23 +42,40 @@ ax = fig.add_axes([margin_inch / fig_width_inch,
                    axis_height_inch / fig_height_inch])
 
 # Erzeuge die Daten und die Funktion
-x = np.linspace(0, 5, 1000)
-y1 = x * x  # Plot (a)
+x = np.linspace(x_min, x_max, 1000)
+y1 = x * x * x # Plot (a)
 
 # Plotten der Funktion
 ax.plot(x, y1, 'r')
 
 # Setze die Limits so, dass die Achsen 3 cm und 5 cm lang sind
-ax.set_xlim([0, 3])
-ax.set_ylim([0, 5])
+ax.set_xlim([x_min, x_max])
+ax.set_ylim([y_min, y_max])
 
-# Setze die Ticks und entferne nur die äußersten Zahlen, nicht die Hilfslinien
-ax.xaxis.set_major_locator(MaxNLocator(integer=True, prune='upper'))  # Entfernt oberste Haupt-Ticks (Zahlen)
-ax.yaxis.set_major_locator(MaxNLocator(integer=True, prune='upper'))  # Entfernt oberste Haupt-Ticks (Zahlen)
 
-# Behalte die Hilfslinien bei
-ax.xaxis.set_minor_locator(AutoMinorLocator(2))  # Hilfslinien (Minor Ticks)
-ax.yaxis.set_minor_locator(AutoMinorLocator(2))  # Hilfslinien (Minor Ticks)
+# X-Achse
+ax.xaxis.set_major_locator(MultipleLocator(grid_spacing_x))  # Hauptticks
+ax.xaxis.set_minor_locator(AutoMinorLocator(2))  # Hilfsticks (jede zweite Hauptlinie)
+
+# Y-Achse
+ax.yaxis.set_major_locator(MultipleLocator(grid_spacing_y))  # Hauptticks
+ax.yaxis.set_minor_locator(AutoMinorLocator(2))  # Hilfsticks (jede zweite Hauptlinie)
+
+# Zeichne den Plot, um die Ticks und Labels korrekt zu generieren
+plt.draw()
+
+# Hole die aktuell gesetzten Ticks
+xticks = ax.get_xticks()
+yticks = ax.get_yticks()
+
+# Setze die Labels manuell, wobei das letzte Label ausgeblendet wird (HOKUSPOKUS)
+xtick_labels = [item.get_text() for item in ax.get_xticklabels()]
+xtick_labels[-2] = ''  # Letztes X-Label entfernen
+ax.set_xticklabels(xtick_labels)
+
+ytick_labels = [item.get_text() for item in ax.get_yticklabels()]
+ytick_labels[-2] = ''  # Letztes Y-Label entfernen
+ax.set_yticklabels(ytick_labels)
 
 # Füge das Gitter hinzu
 ax.grid(which='both')  # Beide, major und minor Ticks
@@ -64,8 +92,8 @@ ax.plot(0, 1, "^k", transform=ax.get_xaxis_transform(), clip_on=False, markersiz
 
 
 # Setze die Achsenbeschriftungen dort, wo die letzte Zahl (3 und 5) wäre
-ax.text(3, -0.2, r'$U \, [\mathrm{V}]$', verticalalignment='top', horizontalalignment='center', fontsize=12)
-ax.text(-0.2, 5, r'$I \, [\mathrm{A}]$', verticalalignment='center', horizontalalignment='right', fontsize=12, rotation=0)
+ax.text(x_max, -0.2, r'$U \, [\mathrm{V}]$', verticalalignment='top', horizontalalignment='center', fontsize=12)
+ax.text(-0.2, y_max, r'$I \, [\mathrm{A}]$', verticalalignment='center', horizontalalignment='right', fontsize=12, rotation=0)
 
 
 # Speichere den Plot ohne unnötige Ränder
